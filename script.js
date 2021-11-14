4b2250b19bfca741b3c847499c730d2876cd92db
Chiara Grottini <chiaragrottini@gmail.com>
2021-11-15 00:20:27 +0100

    implemented select input

--- script.js
+++ script.js
@@ -13,28 +13,33 @@ todoList.addEventListener('click', deleteCheck);
 function addTodo(event){
     //prevent form from submitting
     event.preventDefault();
+
     // create div by clicking
     const todoDiv = document.createElement('div');
-    //add class
-    todoDiv.classList.add('todo');
+    todoDiv.classList.add('todo');//add class
+
     //Create li
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value; //il valore scritto dentro l'input, viene stampato nel LI
     newTodo.classList.add('todo-item');
     //attached li in the div
     todoDiv.appendChild(newTodo);
+
     //COMPLETED BUTTON
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';//check icon
     completedButton.classList.add('completed-btn'); //add class
     todoDiv.appendChild(completedButton); //append to div
+
     //DELETE BUTTON
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; //trash icon
     deleteButton.classList.add('delete-btn'); //add class
     todoDiv.appendChild(deleteButton); //append to div
+
     //APPEND DIV TO LIST
     todoList.appendChild(todoDiv);
+    
     //Clear input value after adding the new LI
     todoInput.value = '';
 }
@@ -44,7 +49,10 @@ const item = e.target;
 //delete
 if(item.classList[0] === 'delete-btn'){
     const todo = item.parentElement;
-   todo.remove();
+    todo.classList.add('fall');  //animation
+    todo.addEventListener('transitionend', function(){ //wait the end of the animation-transition
+        todo.remove(); //remove LI
+    })
 }
 
 //check
