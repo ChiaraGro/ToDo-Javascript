2a8b6bb72a2fe17aca32cf38f1a674ea7108bdfb
Chiara Grottini <chiaragrottini@gmail.com>
2021-11-14 22:48:55 +0100

    style button and input

---
+++ script.js
@@ -0,0 +1,8 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+
+//Functions
